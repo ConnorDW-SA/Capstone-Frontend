2209274bffc4567d277336ac793eba855e80bc0e
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-28 21:43:26 +0200

    board added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Board from "./components/Board";
+function App() {
+  return (
+    <div className="App">
+      <h1>Chess</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
